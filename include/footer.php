832f0df60f06021ecc0243afca64ddf82ed9257b
deveauxsarah <deveauxsarah.dev42@gmail.com>
2022-05-24 18:52:04 +0200

    update items

--- include/footer.php
+++ include/footer.php
@@ -1,26 +1,3 @@
-<!-- <div class="col-sm-6 contact">
-    <h3>E-mail</h3>
-    <p>julienmartin42120@gmail.com</p>
-    <h3>Téléphone</h3>
-    <p>07.60.89.87.01</p>
-    <div class="social_links">
-        <a href="https://codepen.io/rahulsahofficial">
-            <span class="fa-stack fa-lg gt combo">
-                <i class="fa fa-circle fa-stack-2x circle"></i>
-                <i class="fa fa-codepen fa-stack-1x fa-inverse icon"></i>
-            </span>
-        </a>
-        <a href="https://www.linkedin.com/in/julien-martin-770194206/">
-            <span class="fa-stack fa-lg tw combo">
-                <i class="fa fa-circle fa-stack-2x circle"></i>
-                <i class="fa fa-linkedin fa-stack-1x fa-inverse icon"></i>
-            </span>
-        </a>
-    </div>
-</div> -->
-
-
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
